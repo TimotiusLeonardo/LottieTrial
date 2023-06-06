07817857080983fcd347993695fa3d087aa3d03f
Timotius Leonardo <60493932+TimotiusLeonardo@users.noreply.github.com>
2023-06-06 19:16:28 +0700

    Initial Commit

---
+++ LottieTrial/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  LottieTrial
+//
+//  Created by Timotius Leonardo Lianoto on 06/06/23.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+    
+    var animation: LottieAnimationView?
+    lazy var buttonPlay: UIButton = UIButton()
+    let urlInString: [String] = [
+        "https://assets9.lottiefiles.com/packages/lf20_LBnGq7OY4M.json",
+        "https://assets9.lottiefiles.com/packages/lf20_jfXcHs2ED8.json",
+        "https://assets9.lottiefiles.com/packages/lf20_aw5nxeBcYI.json",
+        "https://assets9.lottiefiles.com/packages/lf20_1xU9Bzegk7.json"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(buttonPlay)
+        buttonPlay.translatesAutoresizingMaskIntoConstraints = false
+        buttonPlay.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        buttonPlay.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        buttonPlay.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        buttonPlay.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        buttonPlay.setTitle("Play Animation", for: .normal)
+        buttonPlay.setTitleColor(.blue, for: .normal)
+        buttonPlay.titleLabel?.textAlignment = .center
+        
+        
+        buttonPlay.addTarget(self, action: #selector(onButtonPlayTapped), for: .touchUpInside)
+    }
+    
+    @objc func onButtonPlayTapped() {
+        playRandomAnimation()
+    }
+    
+    func playRandomAnimation() {
+        let url = URL(string: urlInString.randomElement()!)
+        animation = LottieAnimationView(url: url!,
+                                        closure: { _ in
+            // Completions
+        })
+        
+        animation?.animationLoaded = { animationView, animation in
+            animationView.play { [weak self] _ in
+                animationView.removeFromSuperview()
+                self?.animation = nil
+            }
+        }
+        
+        guard let animation = animation else { return }
+        
+        self.view.addSubview(animation)
+        view.layoutIfNeeded()
+        animation.frame = self.view.frame
+    }
+}
+
